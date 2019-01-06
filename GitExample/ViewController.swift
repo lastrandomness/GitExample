b2c398e79517448a772eb51b099c2b4a9b15cb1b
MZA <last.randomness@gmail.com>
2019-01-06 10:08:27 -0800

    second time in M-branch test

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("some Change in M-branch")
+        print("some Change in M-branch second time")
         // Do any additional setup after loading the view.
     }
     
